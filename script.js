2a99f757e295221618a3b3f3ffc5ead713907225
Chayse Stiver <cmstiver1@gmail.com>
2021-06-19 00:57:45 -0700

    Card has basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let myLibrary = []
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -9,18 +9,39 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   //adds to libray array
-  let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
+  let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked)
   myLibrary.unshift(newBook)
-  //adds row to html with book data
-  
+  createRow()
 }
 
-//const bookTitle = document.querySelector("#bookTitle")
-//const bookAuthor = document.querySelector("#bookAuthor")
-//const bookPages = document.querySelector("#bookPages")
-//const bookRead = document.querySelector("#bookRead")
-//const submitButton = document.querySelector("#submitButton")
-//submitButton.addEventListener('click', addBookToLibrary)
+function createRow() {
+  //adds row to html
+  let row = document.createElement('div');
+  row.classList.add('row');
+  tableContainer.prepend(row);
+  myLibrary.forEach(book => {
+    let div = document.querySelector('.row')
+    
+    div.innerHTML = div.innerHTML + `
+      <div class='row'>
+      <div class='data'>Title: ${book.title}</div>
+      <div class='data'>Author: ${book.author}</div>
+      <div class='data'>Pages: ${book.pages}</div>
+      <div class='data'>Completed?: ${book.read}</div>
+      </div>
+      `
+  })
+}
+
+const tableContainer = document.getElementById("tableContainer")
+
+
+const bookTitle = document.querySelector("#bookTitle")
+const bookAuthor = document.querySelector("#bookAuthor")
+const bookPages = document.querySelector("#bookPages")
+const bookRead = document.querySelector("#bookRead")
+const submitButton = document.querySelector("#submitButton")
+submitButton.addEventListener('click', addBookToLibrary)
 
 
 
