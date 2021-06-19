cf25669922606f4d1134bc16d102aa3a64430c9b
Chayse Stiver <cmstiver1@gmail.com>
2021-06-18 22:14:22 -0700

    added function to add books to array

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
+  myLibrary.unshift(newBook)
 }
+
+const bookTitle = document.querySelector("#bookTitle")
+const bookAuthor = document.querySelector("#bookAuthor")
+const bookPages = document.querySelector("#bookPages")
+const bookRead = document.querySelector("#bookRead")
+const submitButton = document.querySelector("#submitButton")
+submitButton.addEventListener('click', addBookToLibrary)
+
+
+
+
